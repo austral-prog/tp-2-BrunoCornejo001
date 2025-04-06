13b020f91263bb418afae659c937ebfa04f1c27c
BrunoCornejo001 <bcornejo@mail.austral.edu.ar>
2025-04-06 20:59:40 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,8 @@
 def earth():
-    x = "Bangladesh"
-    y = "Barbados"
+	x = "Bangladesh"
+	y = "Barbados"
+	condicion = x < y
+	condicion2 = y < x
+	print(f"The result of {x} comes first in the dictionary than {y} is {condicion}.")
+	print(f"The result of {y} comes first in the dictionary than {x} is {condicion2}.")
+earth()
